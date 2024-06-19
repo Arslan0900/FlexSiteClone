70623e31cb8c73b521fd08aff8326953d6736ce9
Arslan0900 <arslankhan2029@gmail.com>
2024-06-19 17:37:37 +0500

    make some changes on navmenu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,75 @@
-import React from 'react'
-import style from "./Style/Navbar.module.scss"
-import { Link } from 'react-scroll'
-import { useState } from 'react'
+import React from 'react';
+import style from "./Style/Navbar.module.scss";
+import { Link } from 'react-scroll';
+import { useState } from 'react';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const toggleBtn =()=>{
+
+  const toggleBtn = () => {
     setOpen(!open);
   }
+
+  const closeMenu = () => {
+    setOpen(false);
+  }
+
   return (
     <>
-    <nav className={style.navbar}>
-      <div className={style.container}>
-        <div className={style.nevManuFlex}>
-      <div className={style.logo}><Link to='Home' spy={true} smooth={true} offset={-100} duration={1000}><img src='./pics/logoColorFull.svg' alt='logo'/></Link>
-      <span className={`${style.burgerIcone} fa fa-bars`} onClick={toggleBtn}></span>
-      </div>
-      <div className={`${open && style.menu2 } ${style.menu}`}>
-        <ul>
-          <li><Link to='Home' spy={true} smooth={true} offset={-100} duration={1000}>Home</Link></li>
-          <li><Link to='Process' spy={true} smooth={true} offset={-100} duration={1000}>Process</Link></li>
-          <li><Link to='Testimonial' spy={true} smooth={true} offset={-100} duration={1000}>Testimonial</Link></li>
-          <li><Link to='Services' spy={true} smooth={true} offset={-100} duration={1000}>Services</Link></li>
-          <li><Link to='Facts' spy={true} smooth={true} offset={-100} duration={1000}>Facts</Link></li>
-          <li><Link to='About' spy={true} smooth={true} offset={-100} duration={1000}>About</Link></li>
-          <li><Link to='Pricing' spy={true} smooth={true} offset={-100} duration={1000}>Pricing</Link></li>
-        </ul>
-      </div>
-      <div className={style.button}>Contect</div>
-      </div>
-      </div>
-    </nav>
+      <nav className={style.navbar}>
+        <div className={style.container}>
+          <div className={style.nevManuFlex}>
+            <div className={style.logo}>
+              <Link to='Home' spy={true} smooth={true} offset={-100} duration={1000}>
+                <img src='./pics/logoColorFull.svg' alt='logo' />
+              </Link>
+              <span className={`${style.burgerIcone} fa fa-bars`} onClick={toggleBtn}></span>
+            </div>
+            <div className={`${open && style.menu2} ${style.menu}`}>
+              <ul>
+                <li>
+                  <Link to='Home' spy={true} smooth={true} offset={-100} duration={1000} onClick={closeMenu}>
+                    <p>Home</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='Process' spy={true} smooth={true} offset={-100} duration={1000} onClick={closeMenu}>
+                    <p>Process</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='Testimonial' spy={true} smooth={true} offset={-100} duration={1000} onClick={closeMenu}>
+                    <p>Testimonial</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='Services' spy={true} smooth={true} offset={-100} duration={1000} onClick={closeMenu}>
+                    <p>Services</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='Facts' spy={true} smooth={true} offset={-100} duration={1000} onClick={closeMenu}>
+                    <p>Facts</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='About' spy={true} smooth={true} offset={-100} duration={1000} onClick={closeMenu}>
+                    <p>About</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='Pricing' spy={true} smooth={true} offset={-100} duration={1000} onClick={closeMenu}>
+                    <p>Pricing</p>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className={style.button}>Contact</div>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
